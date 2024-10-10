14ccf18bdaa67591277dcdaf821ea893fa7d8599
Suhail Abdelaal <souhail.abdelall01@gmail.com>
2024-10-10 16:36:07 +0300

    Solve Game4

--- scripts/win.js
+++ scripts/win.js
@@ -1,21 +1,16 @@
 // add the game address here and update the contract name if necessary
-// game4 = 0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6
-const gameAddr = "0x8A791620dd6260079BF849Dc5567aDC3F2FdC318";
-const contractName = "Game5";
+
+const gameAddr = "0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6";
+const contractName = "Game4";
 
 async function main() {
   // attach to the game
   const game = await hre.ethers.getContractAt(contractName, gameAddr);
 
   // do whatever you need to do to win the game here:
-  const tx1 = await game.giveMeAllowance(10000);
-  await tx1.wait();
-
-  const tx2 = await game.mint(10000);
-  await tx2.wait();
-
-  const tx3 = await game.win();
-  const receipt = await tx3.wait();
+  
+  const tx = await game.win(56);
+  const receipt = await tx.wait();
   console.log(receipt);
 }
 
